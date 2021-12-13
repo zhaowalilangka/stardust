1995992f01ab481279019286e3a482a1b58195f7
zhanglibin <xingfukuaidi@163.com>
2021-12-13 20:35:54 +0800

    v2

--- stringsx/string.go
+++ stringsx/string.go
@@ -1,7 +1,16 @@
 package stringsx
+
 import (
-  "fmt"
+    "fmt"
 )
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s",name)
+
+func Hello(name string, lang string) (string, error) {
+    switch lang {
+    case "en":
+        return fmt.Sprintf("hello %s", name), nil
+    case "zh":
+        return fmt.Sprintf("你好 %s", name), nil
+    default:
+        return "", fmt.Errorf("unknow language ")
+    }
 }
